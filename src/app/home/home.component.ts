515bac44f57cffe323b146302dd4fcf390f00009
SivaBharat <sivabharthece@gmail.com>
2023-06-21 11:29:38 +0530

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  img="assets/img/home.jpg";
+  image="assets/img/home2.jpg";
+  imag="assets/img/home3.jpg"
+}
